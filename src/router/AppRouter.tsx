a01e8263cc8ce401df4461741e5d4d54d886702b
Lucas Murta <lucas111murta@gmail.com>
2025-10-17 17:21:33 -0300

    refactor(test): remove button test page and route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
-import ButtonTest from '../pages/button-test/ButtonTest';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/button-test" element={<ButtonTest />} />
       </Routes>
     </Router>
   );
