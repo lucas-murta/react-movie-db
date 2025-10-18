372502a9c2e1f19f6a6e685c4df2f6c05031074c
Lucas Murta <lucas111murta@gmail.com>
2025-10-18 00:18:49 -0300

    feat(icons): add fontawesome library integration

---
+++ src/lib/base-componente/icon/fontawesome.ts
@@ -0,0 +1,25 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faFilm,
+  faSearch,
+  faHeart,
+  faStar,
+  faPlay,
+  faHome,
+  faUser,
+  faCog,
+  faClapperboard,
+} from '@fortawesome/free-solid-svg-icons';
+
+// Adiciona os ícones à biblioteca
+library.add(
+  faFilm,
+  faSearch,
+  faHeart,
+  faStar,
+  faPlay,
+  faHome,
+  faUser,
+  faCog,
+  faClapperboard
+);
--- src/lib/base-componente/index.ts
+++ src/lib/base-componente/index.ts
@@ -2,3 +2,4 @@ export { Button } from './button';
 export { Input } from './input';
 export { Select } from './select';
 export { Chips } from './chips';
+export * from './icon';
